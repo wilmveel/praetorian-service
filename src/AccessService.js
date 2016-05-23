33442f6e7efbcc988cde312156edd255149045a6
axelpoise <a.r.scheele@live.com>
2016-05-23 19:48:20 +0200

    access service

---
+++ src/AccessService.js
@@ -0,0 +1,29 @@
+module.exports = function(contract){
+    return {
+        
+        find:function(callback){
+            contract.getAccess(function(err, access){
+                callback(err, access);
+            })
+        },
+        
+        create:function(callback){
+            contract.createAccess.estimateGas(function(err, gas){
+                if(err) return callback(err);
+                contract.createAccess({
+                    gas: (gas * 2)
+                }, function(err, transactionHash) {
+                    if(err) return callback(err);
+                    var events = contract.allEvents();
+                    events.watch(function (err, event){
+                        if(err) return callback(err);
+                        if(event && event.transactionHash == transactionHash){
+                            events.stopWatching();
+                            callback(null, event.args.addr);
+                        }
+                    })
+                })
+            })
+        }
+    }
+}
---
+++ test/unit/AccessService.spec.js
@@ -0,0 +1,54 @@
+
+var assert = require('assert');
+var Helper = require('../Helper');
+var AccessService = require('../../src/accessService');
+
+describe('services', function () {
+
+    var helper = new Helper(this);
+
+    var service;
+    
+    var delegate;
+
+    before(function (done) {
+        helper.init(done)
+    });
+
+    before(function (done) {
+        helper.deploy(function (err, contract) {
+            if (err) return done(err)
+            service = new AccessService(contract);
+            done();
+        });
+    });
+    
+    it('should return a hexidecimal zero', function(done){
+        service.find(function(err, address){
+            if(err) return done(err);
+            assert(address.toString('hex') === '0x0000000000000000000000000000000000000000');
+            console.log(address);
+            done();
+        })
+    })
+    
+    it('should create an access contract', function(done){
+        service.create(function(err, address){
+            if(err) return done(err);
+            console.log("Address", address);
+            
+            done();
+        })
+    });
+    
+    it('should give the access contract', function(done){
+        service.find(function(err, access){
+            if(err) return done(err);
+            assert(access.toString('hex') !== '0x0000000000000000000000000000000000000000');
+            console.log(access);
+            done();
+        })
+    });
+    
+    
+})
